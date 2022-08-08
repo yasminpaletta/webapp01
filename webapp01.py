aecfe54f9cd2417bd5befa0c2f2d8ba3de471a34
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2022-08-08 17:19:20 -0300

    Add files via upload

---
+++ webapp01.py
@@ -0,0 +1,52 @@
+# myFirstStreamlitApp.py
+#import the libraries
+import streamlit as st
+from PIL import Image
+
+image01 = Image.open('desenvolvimento.jpg')
+# Use st.title("") para adicionar um TÍTULO ao seu Web app
+st.title("MEU 1º WEB APP STREAMLIT")
+
+# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
+st.header("Cabeçalho")
+
+# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
+st.subheader("Sub Cabeçalho")
+
+# Use st.write("") para adicionar um texto ao seu Web app
+st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+
+st.subheader("------ **Desenvolvido por: Massaki de O. Igarashi** -----")
+
+menu = ["Texto_Colunas",
+        "Texto_Markdown",
+        "Inserir_Figura"]
+choice = st.sidebar.selectbox("Menu de Opções",menu)
+st.sidebar.write("Texto Side Bar")
+    
+if choice == "Texto_Colunas":       
+    st.subheader("Texto formatado em colunas")
+    st.write("Veja a seguir uma opção de formatação em colunas")    
+    cols01 = st.columns(2)    
+    cols01[0].write('Texto da Coluna 01')
+    cols01[1].write('Texto da Coluna 02')
+elif choice == "Texto_Markdown":
+    st.subheader("Texto Markdown")
+    st.write("Veja a seguir opção de formatação de texto Markdown")
+    st.markdown(
+    """
+    ## *Alguns sites referências*:    
+    - [Streamlit: hello world](https://calmcode.io/streamlit/hello-world.html)
+    - [:star: Streamlit emoji shortcodes](https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlitapp.com/)
+    - [Layouts and Containers](https://docs.streamlit.io/library/api-reference/layout)
+   
+    ##### CRONOGRAMA
+    DIA | CH HORÁRIA | CONTEÚDO
+    :---------: | :------: | :-------:
+    Dia 1 de 2 | ?h | ? a ?
+    Dia 2 de 2 | ?h | ? a ?
+    """
+    )
+elif choice == "Inserir_Figura":
+    st.image(image01, width=800, caption='Rótulo da Imagem 01') 
+    
