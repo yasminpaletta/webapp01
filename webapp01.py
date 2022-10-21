3def7ef19a005f1236877ab127e5b8dae8151574
yasminpaletta <116382247+yasminpaletta@users.noreply.github.com>
2022-10-21 16:48:08 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -8,10 +8,10 @@ image01 = Image.open('panda.jpg')
 st.title("Bem vindo, EMEF/EJA Edson Luis Lima Souto!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("passeio escolar para a univercidade mackensie")
+st.header("passeio escolar para a universidade mackensie")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("muito aprendizado")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
