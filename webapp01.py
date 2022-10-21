10bc54abfaf44e08f7b37e03e043f2e1ab14f371
yasminpaletta <116382247+yasminpaletta@users.noreply.github.com>
2022-10-21 16:38:47 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -3,7 +3,7 @@
 import streamlit as st
 from PIL import Image
 
-image01 = Image.open('desenvolvimento.jpg')
+image01 = Image.open('panda.jpg')
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("Bem vindo, EMEF/EJA Edson Luis Lima Souto!")
 
